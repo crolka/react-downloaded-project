bece6563ea11ed7e667f85590914ce3c1a2f467a
crolka <christinemrolka@gmail.com>
2022-06-28 02:40:21 -0400

    Reacting to Events
    In the ExpenseItem.js we called the useState Function in order to keep track of changes in the code's value.
    
    This method will trigger an application update when the state changes.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
@@ -12,13 +12,19 @@ import "./ExpenseItem.css";
 // THe ExpenseDate component is a sub component to which we pass
 // the expenseDate property along.
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
 
+  const clickHandler = () => {
+    let newTitle = prompt("What is the new title? ");
+    setTitle(newTitle);
+  };
   return (<Card className="expense-item">
     <ExpenseDate expenseDate={props.expenseDate} />
     <div className="expense-item__description">
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
     </div>
     <div className="expense-item__price">${props.amount}</div>
+    <button onClick={clickHandler}>Change Title!</button>
   </Card>);
 }
 
